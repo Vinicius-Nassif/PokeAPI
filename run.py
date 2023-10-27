ebbf4924d617ac216ec7b05efb8d76421cf275d8
Vinicius-Nassif <vinicius.nassif@gmail.com>
2023-10-27 18:08:26 -0300

    alteracoes

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
+import os
 from app import app, db
 
 def main():
     # Inicializa o objeto SQLAlchemy associado à aplicação
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
+    
+    # Verifica se o arquivo do banco de dados já existe
+    db_file = app.config['SQLALCHEMY_DATABASE_URI'].replace('sqlite:///','')
+    if not os.path.exists(db_file):
+        with app.app_context():
+            db.create_all()
+            print('Banco de dados criado com sucesso!')
+    else:
+        print('O arquivo do banco de dados já existe.')
 
     # Inicia o servidor de desenvolvimento do Flask com depuração ativada
     app.run(debug=True)
